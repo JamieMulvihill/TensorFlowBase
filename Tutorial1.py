1249f21b5a1af466a5fb6acdc70c55981cae6fbf
JamieMulvihill <44208828+JamieMulvihill@users.noreply.github.com>
2024-07-08 20:20:43 +0100

    added test case and predictions

--- Tutorial1.py
+++ Tutorial1.py
@@ -1,11 +1,14 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 print("TensorFlow version:", tf.__version__)
 print("Hello")
 
+# Load and preprocess the MNIST dataset
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
+# Build the model
 model = tf.keras.models.Sequential([
   tf.keras.layers.Flatten(input_shape=(28, 28)),
   tf.keras.layers.Dense(128, activation='relu'),
@@ -13,17 +16,53 @@ model = tf.keras.models.Sequential([
   tf.keras.layers.Dense(10)
 ])
 
-predictions = model(x_train[:1]).numpy()
-predictions
-
-tf.nn.softmax(predictions).numpy()
-
+# Define the loss function
 loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-loss_fn(y_train[:1], predictions).numpy()
-
+# Compile the model
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
+# Train the model
+history = model.fit(x_train, y_train, epochs=5, validation_split=0.2)
+
+# Plot training & validation accuracy values
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model accuracy')
+plt.ylabel('Accuracy')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Plot training & validation loss values
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model loss')
+plt.ylabel('Loss')
+plt.xlabel('Epoch')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=2)
+print(f'\nTest accuracy: {test_accuracy}')
+
+# Predicting on the first 5 test images
+predictions = model.predict(x_test[:5])
+
+# Converting logits to probabilities
+probabilities = tf.nn.softmax(predictions).numpy()
+
+# Getting the predicted classes
+predicted_classes = tf.argmax(probabilities, axis=1).numpy()
+
+# Plotting the results
+plt.figure(figsize=(10, 2))
+for i in range(5):
+    plt.subplot(1, 5, i+1)
+    plt.imshow(x_test[i], cmap='gray')
+    plt.title(f"True: {y_test[i]}\nPred: {predicted_classes[i]}")
+    plt.axis('off')
+plt.show()
